55af9663b17f0fbe2258cc075b9322c24ec1b49c
xiao tu zi <sunjie20081001@126.com>
2015-07-21 17:11:17 +0800

    完成

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
 /* global chrome */
-chrome.app.runtime.onLaunched.addListenner(function(){
-	chrome.app.window.create('window.html',{
-		
-	});
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('window.html', {
+    'id':'fanqie',
+    'frame':{
+      'type': 'none'
+    },
+    'outerBounds': {
+      'width': 320,
+      'height': 320,
+      'maxWidth':320,
+      'minHeight':320
+    },
+    'resizable': false
+  });
 });
---
+++ js/app.js
@@ -0,0 +1,86 @@
+$(function(){
+	//运行app
+	var state = "stop";
+	var step  = 60;  //时间步长
+	var time  = step * 25;   //秒计算,时长
+	var lefttime = time;
+	var timeIn = null;
+	
+	function render(){
+		var m = parseInt(lefttime/step);
+		var s = lefttime - m*step;
+		if(m < 10){
+			m = "0"+m;
+		}
+		if(s < 10){
+			s = "0"+s;
+		}
+		$('.j-time').html(m+":"+s);	
+	}
+	
+	render();
+	
+	
+	function run(){
+		if(state == 'stop'){
+			state = 'run';
+			timeIn = setInterval(function(){
+			if(lefttime == 0){
+				state = "stop";
+				clearInterval(timeIn);
+			}else{
+				lefttime--;
+				render();
+			}
+		},1000);
+		}
+	}
+	function stop(){
+		if(timeIn){
+		clearInterval(timeIn);
+		state = 'stop';
+		}
+	}
+	
+	function reset(){
+		if(timeIn){
+			stop();
+		}
+		lefttime =time;
+		render();
+	}
+	
+	$('.j-add').click(function(){
+		//增加时长
+		if(state == "stop"){
+			time += step;
+			lefttime = time;
+			render();
+			console.log();
+		}
+	});
+	
+	$('.j-minus').click(function(){
+		if(state == 'stop' && time > 0){
+			time -= step;
+			if(time < 0){
+				time = 0;
+			}
+			lefttime = time;
+			render();
+		}
+	});
+	
+	$('.j-start').click(function(){
+		run();
+	});
+	
+	$('.j-stop').click(function(){
+		stop();
+	});
+	
+	$('.j-reset').click(function(){
+		reset();
+	});
+	
+})
